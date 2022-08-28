ad605f40c98e9b3b6c8193ec13ccb80d680cf2ce
NenadGeorgievski <nenad.georgievski.ku@hotmail.com>
2022-08-28 12:51:47 +0200

    Finished homework for lecture on 26.08.22

---
+++ Homework/Homework_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Please enter the first number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter the second number:");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter the third number:");
+int num3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter the fourth number:");
+int num4 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine($"The product of {num1} * {num2} * {num3} * {num4} = {num1 * num2 * num3 * num4}");
