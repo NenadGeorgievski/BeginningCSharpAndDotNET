c663e7db47232544ce773cf17e97de6a11a20352
NenadGeorgievski <nenad.georgievski.ku@hotmail.com>
2022-09-02 20:50:58 +0200

    Practicing boolean logic and logical operators

---
+++ Chapter04/Ch04xEx4.1/Ch04Ex4.1/Program.cs
@@ -0,0 +1,22 @@
+﻿int var1 = 51, var2 = 2;
+
+if((var1 > 10) ^ (var2 > 10))
+{
+    Console.WriteLine("One of these numbers is greater than 10, but not both of them.");
+} else
+{
+    Console.WriteLine("The condition is not matched.");
+}
+
+int num1, num2;
+
+do
+{
+    Console.WriteLine("Please enter two numbers, one of them being greater than 10.");
+    Console.WriteLine("Enter the first number: ");
+    num1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter the second number ");
+    num2 = Convert.ToInt32(Console.ReadLine());
+} while (!((num1 > 10) ^ (num2 > 10)));
+
+Console.WriteLine($"You have entered the correct format of numbers.");
