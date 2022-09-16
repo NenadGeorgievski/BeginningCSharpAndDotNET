b8c01b96429b779672e3ac0138d3ef91dfe4d25b
NenadGeorgievski <nenad.georgievski.ku@hotmail.com>
2022-09-16 12:59:27 +0200

    Working with functions, chapter 06

--- Chapter06/Ch06Ex03/Program.cs
+++ Chapter06/Ch06Ex03/Program.cs
@@ -19,6 +19,26 @@ namespace Ch06Ex03
         {
             int sum = SumVals(1, 5, 2, 9, 8);
             Console.WriteLine($"The sum of all the values is: {sum}");
+
+            int[] myArray = { 1, 8, 2, 3, 5, 3, 4, 6, 4, 6, 3, 5 };
+            Console.WriteLine($"The maximum value in myArray is: {MaxValue(myArray, out int maxIndex)}");
+            Console.WriteLine($"The first occurence of this value is at {maxIndex + 1}");
+
+            static int MaxValue(int[] intArray, out int MaxIndex)
+            {
+                int maxValue = intArray[0];
+                MaxIndex = 0;
+                for (int i = 1; i < intArray.Length; i++)
+                {
+                    if (intArray[i] > maxValue)
+                    {
+                        maxValue = intArray[i];
+                        MaxIndex = i;
+                    }
+                   
+                }
+                return maxValue;
+            }
         }
     }
 }
