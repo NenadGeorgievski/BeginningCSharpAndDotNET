57c970e52866f701daf90aa7feeef4cf7dbe9fc6
NenadGeorgievski <nenad.georgievski.ku@hotmail.com>
2022-09-23 16:08:39 +0200

    Started working with classes and inheritance

---
+++ Objects/Objects/Cow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Objects
+{
+    public class Cow : Animal
+    {
+      
+        public Cow() : base()
+        {
+
+        }
+
+        public Cow(string name, int age) : base(name, age)
+        {
+
+        }
+        public void SupplyMilk()
+        {
+            Console.WriteLine($"{Name} is lactating.");
+        }
+
+        public void Moo()
+        {
+            Console.WriteLine($"{Name} is mooing.");
+        }
+
+    }
+
+}
---
+++ Objects/Objects/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chicken chicken = new Chicken("Marko D Great", 12);
+            chicken.Age += 1;
+
+            chicken.Breed();
+            chicken.Cluck();
+            Console.WriteLine($"{chicken.Age} is the age of {chicken.Name}.");
+
+            Chicken chicken1 = new Chicken();
+            chicken1.Name = "Goran d Small";
+            chicken1.Age = 1;
+
+            chicken1.Cluck();
+            chicken1.EatFood();
+
+            Cow cow = new Cow("Milka", 12);
+
+            cow.EatFood();
+            cow.Moo();
+
+            Animal newChicken = new Chicken("Iva",3);
+            newChicken.EatFood();
+
+            IAnimal newChickenInt = new Chicken(); ;
+            newChicken.Name = "Dina";
+            newChicken.Age = 1;
+
+            newChickenInt.EatFood();
+
+        }
+    }
+
+}
