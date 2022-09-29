ea3cfef0043bd1cdb8c3b3d132de25faddbb41ba
NenadGeorgievski <nenad.georgievski.ku@hotmail.com>
2022-09-29 16:31:19 +0200

    Finished chapter 9 and started chapter 10

---
+++ Chapter09/Ch09Ex03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ch09Ex03
+{
+    class MyClass
+    {
+        public int val;
+    }
+
+    struct MyStruct
+    {
+        public int val;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass objA = new MyClass();
+            MyClass objB = objA;
+            objA.val = 10;
+            objB.val = 20;
+            MyStruct structA = new MyStruct();
+            MyStruct structB = structA;
+            structA.val = 30;
+            structB.val = 40;
+            Console.WriteLine($"objA.val = {objA.val}");
+            Console.WriteLine($"objB.val = {objB.val}");
+            Console.WriteLine($"structA.val = {structA.val}");
+            Console.WriteLine($"structB.val = {structB.val}");
+        }
+    }
+}
---
+++ Chapter10/Ch10Ex01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ch10Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating object myObj..");
+            MyClass myObj = new MyClass("MyObject");
+            Console.WriteLine("MyObject created..");
+            for (int i = -1; i <= 0; i++)
+            {
+                try
+                {
+                    Console.WriteLine($"Attempting to assign {i} to myObj.Val...");
+                    myObj.Val = i;
+                    Console.WriteLine($"Value {myObj.Val} assigned to myObj.Val..");
+                } catch (Exception e)
+                {
+                    Console.WriteLine($"Exception {e.GetType().FullName} thrown.");
+                    Console.WriteLine($"Message: \n\"{e.Message}\"");
+                }
+            }
+            Console.WriteLine("\nOutputting MyObj.ToString()");
+            Console.WriteLine(myObj.ToString());
+            Console.WriteLine("myObj.ToString() Output.");
+            Console.WriteLine("\nMyDoubledInt = 5");
+            Console.WriteLine($"Getting MyDoubledIntProp of 5 is {myObj.MyDoubledIntProp}");
+            Console.ReadKey();
+        }
+    }
+}
