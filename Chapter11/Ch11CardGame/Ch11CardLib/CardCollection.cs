b884275b220f6187dad5ef1a99b3643ad3927e88
NenadGeorgievski <nenad.georgievski.ku@hotmail.com>
2022-10-07 11:19:04 +0200

    Converting previous exercises to collections

---
+++ Chapter11/Ch11CardGame/Ch11CardLib/CardCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11CardLib
+{
+    public class CardCollection : CollectionBase
+    {
+        public void Add(Card newCard) => List.Add(newCard);
+        public void Remove(Card oldCard) => List.Remove(oldCard);
+
+        public Card this[int cardIndex]
+        {
+            get { return (Card)List[cardIndex]; }
+            set { List[cardIndex] = value; }
+        }
+
+        public void CopyTo(CardCollection targetCards)
+        {
+            for (int index = 0; index < this.Count; index++)
+            {
+                targetCards[index] = this[index];
+            }
+        }
+
+        public bool Contains(Card card) => InnerList.Contains(card);
+    }
+}
---
+++ Chapter11/Ch11Ex02/Program.cs
@@ -0,0 +1,19 @@
+﻿using Ch11Ex02;
+
+AnimalCollection animalCollection = new();
+animalCollection.Add(new Chicken("Donna"));
+animalCollection.Add(new Cow("Milka"));
+
+foreach (Animal animal in animalCollection)
+{
+    animal.Feed();
+}
+
+animalCollection[1].Name = "Noah";
+
+foreach (Animal animal in animalCollection)
+{
+    animal.Feed();
+}
+
+((Chicken)animalCollection[0]).LayEgg();
